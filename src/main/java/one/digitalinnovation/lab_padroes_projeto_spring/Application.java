412de4a583c618fe9871a231dc87f92e3d12072e
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-10-19 10:18:31 -0300

    Design patterns with Spring
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

---
+++ src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/Application.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.lab_padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
---
+++ src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/service/ClienteService.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.service;
+
+import one.digitalinnovation.lab_padroes_projeto_spring.model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ *
+ */
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
